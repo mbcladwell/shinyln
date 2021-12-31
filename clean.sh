ff5a0aabe98644672dce6c8cd68a577a16e8e6e9
mbcladwell <mbcladwell@gmail.com>
2021-12-31 15:21:59 -0500

    changes

--- clean.sh
+++ clean.sh
@@ -22,7 +22,8 @@ make dist
 
 git add .
 git commit -am "changes"
+git push
 ##scp -i /home/mbc/labsolns.pem ./shinyln-0.1.tar.gz admin@ec2-18-189-31-114.us-east-2.compute.amazonaws.com:.
 ##scp -i /home/mbc/labsolns.pem /home/mbc/syncd/tobedeleted/shinyln/guix.scm admin@ec2-18-189-31-114.us-east-2.compute.amazonaws.com:.
-guix package --install-from-file=guix.scm
+##guix package --install-from-file=guix.scm
 ###source /home/mbc/.guix-profile/etc/profile
